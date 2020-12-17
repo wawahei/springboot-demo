4b47396a3924817f9ec28cdef9a0467f111e213c
wawahei <675756166@qq.com>
2020-12-17 16:48:12 +0800

    add logback-spring

--- src/main/java/com/wawahei/demo/controller/HelloController.java
+++ src/main/java/com/wawahei/demo/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.wawahei.demo.controller;
 
 import com.wawahei.demo.bean.User;
 import com.wawahei.demo.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
  **/
 @Controller
 public class HelloController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
     @Autowired
     private UserService userService;
@@ -33,6 +37,10 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello(Model model){
         model.addAttribute("welcome","welcome you come");
+        LOGGER.debug("进入test方法");
+        LOGGER.info("进入test方法");
+        LOGGER.warn("进入test方法");
+        LOGGER.error("进入test方法");
         return "hello";
     }
 }
